5e84d81a8ce57e3bd5338e442d4149e35a5a9750
lillivoorhies <55668942+lillivoorhies@users.noreply.github.com>
2019-10-09 00:05:20 -0500

    Update songs.js

--- songs.js
+++ songs.js
@@ -9,7 +9,58 @@ let songs =
 		year: '',
 		category: null,
 		lyrics: `J’ai arrivé hier au soir (z)à La maison,
+J’ai cogné, j’ai crié, j’ai let songs = 
+[
+{
+		
+		title: "Reve du Soulard / Drunkard's Dream",
+		key: "",
+		writtenBy: "Cleveland Crochet?",
+		recordedBy:"Cleveland Crochet and the Sugar Bees",
+		year: '',
+		category: null,
+		lyrics: `J’ai arrivé hier au soir (z)à La maison,
 J’ai cogné, j’ai crié, j’ai pas de réponse.
+J’ai connu (z)au moment que t’étais pas là,
+Quel espoir, quel avenir mais moi j’peux avoir 
+
+
+Bambocheur, trainailleur et gros gambler,
+C’est tout j’ai été tous mes jours jusqu’à j’te rejoins.
+J’ai promis, chère ‘tite fille, de plus faire ça,
+C’est si dur, c’est d’connaître mais t’es pas là.`,
+		chordSections: [
+			{
+				name: "",
+				chordBars: ["", ""]
+			}
+		]},
+		
+		{	
+		title: "'Tit Yeux Noirs / Little Dark Eyes	",
+		key: "",
+		writtenBy: "ANON?",
+		recordedBy:"",
+		year: '',
+		category: null,
+		lyrics: `À ce matin je m’ai trouvé assis dessus mon lit
+J’ai pleuré avec un cœur aussi cassé
+Parce que j’avais rêvé à mes chères 'tits yeux noirs
+Alle a parti, a’ va jamais s’en revenir
+ 
+
+Après-midi j’étais assis  dessus ma galerie
+Après guetté le soleil après se coucher
+Je m'ennuyé et j’endors des mes chères 'tits yeux noirs
+C’est ça qui fait j’ai mon cœur tout cassé `,
+		chordSections: [
+			{
+				name: "",
+				chordBars: ["", ""]
+			}
+		]}];
+		
+pas de réponse.
 J’ai connu (z)au moment que t’étais pas là,
 Quel espoir, quel avenir mais moi j’peux avoir 
 
