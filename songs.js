0cb41d8004642149c8d265bae39d4af1d05373ab
lillivoorhies <55668942+lillivoorhies@users.noreply.github.com>
2019-10-09 09:09:21 -0500

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,6 +1,32 @@
 let songs = 
 [
 	{
+		
+		title: "Reve du Soulard / Drunkard's Dream",
+		key: "",
+		writtenBy: "Cleveland Crochet?",
+		recordedBy:"Cleveland Crochet and the Sugar Bees",
+		year: '',
+		category: null,
+		lyrics: `
+			J’ai arrivé hier au soir (z)à La maison,
+			J’ai cogné, j’ai crié, j’ai pas de réponse.
+			J’ai connu (z)au moment que t’étais pas là,
+			Quel espoir, quel avenir mais moi j’peux avoir.
+
+
+			Bambocheur, trainailleur et gros gambler,
+			C’est tout j’ai été tous mes jours jusqu’à j’te rejoins.
+			J’ai promis, chère ‘tite fille, de plus faire ça,
+			C’est si dur, c’est d’connaître mais t’es pas là.
+			`,
+		chordSections: [
+			{
+				name: "",
+				chordBars: ["", ""]
+			}
+		]},
+	{
 		title: "Junk",
 		key: "F#",
 		writtenBy: "The Beatles",
